7ded827ef5a5b06b815deb772420170a3d15158b
liyi <liyi18581720195@icloud.com>
2016-05-12 15:48:42 +0800

    add 4

--- 123/js/123.js
+++ 123/js/123.js
@@ -1,4 +1,5 @@
  window.onload=function () {
+
      var page1 = document.getElementById("page1");
      var page2 = document.getElementById("page2");
      var page3 = document.getElementById("page3");
@@ -40,6 +41,13 @@ audio.addEventListener("ended", function(event) {
     page1.addEventListener("touchstart",function(event){
         page1.style.display = "none";
         page2.style.display = "block";
+        page3.style.display = "block";
+        page3.style.top     = "100%"
+
+        setTimeout(function() {
+            page2.setAttribute("class","page fadeOut")
+            page3.setAttribute("class","page fadeIn")
+        },5500);
     }, false); 
 
  };
